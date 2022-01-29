d138c8011e795ddabec0bbf5221250b360b8f4ed
mahmoud-elnaggar <3bdoelnaggar@gmail.com>
2022-01-29 17:30:01 +0200

    play with constraint_layout_fragment.xml

--- app/src/main/java/com/elnaggar/catalog/ui/notifications/ConstraintLayoutFragment.kt
+++ app/src/main/java/com/elnaggar/catalog/ui/notifications/ConstraintLayoutFragment.kt
@@ -4,9 +4,8 @@ import androidx.fragment.app.Fragment
 import com.elnaggar.catalog.R
 import com.elnaggar.catalog.databinding.ConstraintLayoutFragmentBinding
 
-class NotificationsFragment : Fragment(R.layout.constraint_layout_fragment) {
+class ConstraintLayoutFragment : Fragment(R.layout.constraint_layout_fragment) {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
     private var _binding: ConstraintLayoutFragmentBinding? = null
 
     // This property is only valid between onCreateView and
